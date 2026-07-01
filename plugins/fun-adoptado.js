7ed7d83ac1b83aaf44e46a2982a69010f28c12dc
YALLICO OWNER <yallico2024@gmail.com>
2026-06-30 20:59:57 -0500

    Actualizar fun-adoptado.js

--- plugins/fun-adoptado.js
+++ plugins/fun-adoptado.js
@@ -1,12 +1,14 @@
 let handler = async (m, { conn, mentionedJid }) => {
   if (!mentionedJid?.[0]) return m.reply('Taggea a tu víctima: `.adoptado @user`')
 
-  let quien = mentionedJid[0]
-  let numero = quien.split('@')[0]
+  let quien = mentionedJid[0] // ej: 254417471316137@s.whatsapp.net
+  let numero = quien.split('@')[0] // ej: 254417471316137
 
-  let texto = `*@+${numero}* *ES/IS* *%* *ADOPTADO*_\n_Sus padres se fueron x pañales 😞😂_`
+  // ARREGLO: El @ va pegado al numero SIN el +
+  let texto = `@${numero} *ES/IS* *%* *ADOPTADO*\n_Sus padres se fueron x pañales 😞😂_`
 
   await conn.sendMessage(m.chat, { react: { text: '💀', key: m.key }})
+  // ARREGLO: Pasamos el JID completo en mentions para que se pinte azul
   await conn.reply(m.chat, texto, m, { mentions: [quien] })
 }
 
