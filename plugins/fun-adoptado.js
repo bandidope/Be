df454d9378a5453054dd06df577cd931004c8a98
YALLICO OWNER <yallico2024@gmail.com>
2026-06-30 21:03:02 -0500

    Actualizar fun-adoptado.js

--- plugins/fun-adoptado.js
+++ plugins/fun-adoptado.js
@@ -1,14 +1,16 @@
-let handler = async (m, { conn, mentionedJid, quoted }) => {
-  // [FIX 1] Acepta tag, respuesta, o número
-  let who = mentionedJid?.[0]? mentionedJid[0] : quoted? quoted.sender : null;
+let handler = async (m, { conn, text, mentionedJid, quoted }) => {
+  // [FIX] Detecta: 1.Mencion azul 2.Respuesta 3.Texto @numero
+  let who = mentionedJid?.[0]
+   ? mentionedJid[0]
+    : quoted
+   ? quoted.sender
+    : text?.match(/@(\d+)/)?.[1] + '@s.whatsapp.net';
 
   if (!who) return m.reply(`*Taggea a tu víctima*\n\nEjemplo:.adoptado @user\nO responde a su msj +.adoptado`);
 
   let numero = who.split('@')[0];
 
-  // [FIX 2] Cero _ pegados. Todo limpio con \n y espacios
-  let texto = `@${numero} *ES/IS* *%* *ADOPTADO*\n` +
-              `_Sus padres se fueron x pañales 😞😂_`;
+  let texto = `@${numero} *ES/IS* *%* *ADOPTADO*\n_Sus padres se fueron x pañales 😞😂_`;
 
   await conn.sendMessage(m.chat, { react: { text: '💀', key: m.key }});
   await conn.reply(m.chat, texto, m, { mentions: [who] });
